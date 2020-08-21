2e785cbf7e795cb35d793d81f9f2230469487c31
j-tan871 <60245545+j-tan871@users.noreply.github.com>
2020-08-20 21:08:48 -0500

    update

--- sql_workshop.sql
+++ sql_workshop.sql
@@ -1,29 +1,46 @@
 USE sqlworkshop;
+
+SELECT * FROM customers;
+
+SELECT CustomerID, CustomerName FROM customers;
+
+SELECT CustomerName
+FROM customers
+WHERE CustomerID = 4;
+
+SELECT *
+FROM customers
+WHERE OrderNumber > 105;
+
+SELECT OrderNumber
+FROM customers
+WHERE CustomerName = 'Shaggy Rogers';
+
 SELECT * FROM cholera;
 
 /* Select country and fatality rate columns from the cholera table */
 SELECT country, fatality_rate 
 FROM cholera;
 
-/* Select where year is 2009*/
-SELECT * 
-FROM cholera 
-WHERE year=2009;
-
 /* Select where year is 2009 and in the Eastern Mediterranean region*/
 SELECT * 
 FROM cholera
 WHERE year=2009 AND who_region="Eastern Mediterranean";
 
-/* Select where year is 2009 or region is Africa, and not Eastern Mediterranean */
+/* Select where year is 2000 or 2001 in Africa (incorrect because no parentheses) */
 SELECT *
 FROM cholera
-WHERE year=2009 OR who_region="Africa" AND NOT "Eastern Mediterranean";
+WHERE year = 2000 OR year = 2001 AND who_region = "Africa";
 
-/* Select where year is 2009 or region is Africa, and not Eastern Mediterranean, and order by decreasing fatality rate*/
+/* Select where year is 2000 or 2001 in Africa, and order by decreasing fatality rate */
 SELECT *
 FROM cholera
-WHERE year=2009 OR who_region="Africa" AND NOT "Eastern Mediterranean"
+WHERE ((year = 2000) OR (year = 2001)) AND who_region = "Africa";
+
+/* Select where year is 2000 or 2001 in Africa,  and order by decreasing fatality rate*/
+SELECT *
+FROM cholera
+WHERE year = 2000 OR year = 2001 AND who_region = "Africa"
 ORDER BY fatality_rate DESC;
 
 /* Find the highest number of reported cases in Africa in 2007 */
@@ -34,7 +51,7 @@ WHERE year=2007 AND who_region="Africa";
 /* Find the number of countries with a fatality rate less than 0.5 in 2009 */
 SELECT COUNT(fatality_rate)
 FROM cholera
-WHERE fatality_rate<0.5 AND year=2009;
+WHERE fatality_rate < 0.5 AND year=2009;
 
 /* Find the average fatality rate of countries in 2009 */
 SELECT AVG(fatality_rate)
@@ -56,11 +73,18 @@ SELECT COUNT(reported_deaths), country
 FROM cholera
 GROUP BY country;
 
-/* Find the number of dogs bought by each customer */
+/* Find the number of dogs bought by each customer (Inner Join) */
 USE sqlworkshop;
 SELECT customers.CustomerName, COUNT(dogs.Breed)
 FROM customers
-INNER JOIN dogs ON customers.CustomerID = dogs.CustomerID
+INNER JOIN dogs ON customers.OrderNumber = dogs.OrderNumber
 GROUP BY customers.CustomerName;
 
+/* Show the Customer Name, ID, order number, and breed (Left Join) */
+USE sqlworkshop;
+SELECT customers.CustomerName, customers.CustomerID, customers.OrderNumber, dogs.Breed
+FROM customers
+LEFT JOIN dogs
+ON customers.OrderNumber = dogs.OrderNumber;
 
+/* more resources: https://dev.mysql.com/doc/refman/8.0/en/database-use.html */
